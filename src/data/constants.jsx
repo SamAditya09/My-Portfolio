eb9cf0bb8d992d01c064e745dbe9cc831a656ea2
Aditya Kumar Samantaray <adityasamantaray985@gmail.com>
2023-09-23 10:54:30 +0530

    done

--- src/data/constants.jsx
+++ src/data/constants.jsx
@@ -6,7 +6,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/SamAditya09",
-  resume: "https://drive.google.com/file/d/1xTcDLeZYhJ8RYpOf7PSoXDDCOsZ1kfWx/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1Qo9cfqaBGQljmqMgqlXFljqWnwjO7Jdj/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/aditya-samantaray-207169257/",
   twitter: "https://twitter.com/AdityaS75206848",
   insta: "https://www.instagram.com/sam.aditya19/",
